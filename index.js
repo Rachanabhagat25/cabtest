f7d6a5da2248eaf37ca134b27658bab097b86aac
Rachanabhagat25 <166331231+Rachanabhagat25@users.noreply.github.com>
2024-09-24 18:03:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 const MONGOURL = process.env.MONGO_URL;
 
 const corsOptions = {
-    origin: 'http://localhost:3000', 
+    origin: '*',
     optionsSuccessStatus: 200
 };
 
